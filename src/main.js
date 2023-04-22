7ad10c3f0780c64f12e6cfd7b8bb0f04e1448c83
tkubata <tkubata9@gmail.com>
2023-04-22 21:39:26 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-sessionStorage.setItem('test new key', 'value')
+let transitionTrigger = document.querySelector('.transition-trigger')
+let transition = document.querySelector('.transition')
+
+window.addEventListener('load', () => {
+  if (sessionStorage.length === 0) {
+    console.log('hello')
+  } else {
+    console.log('Goodbye')
+  }
+})
