d523c1221f28c039c3ea11342b0e9523f61f61da
tkubata <tkubata9@gmail.com>
2023-04-22 21:44:57 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ let transition = document.querySelector('.transition')
 
 window.addEventListener('load', () => {
   if (sessionStorage.length === 0) {
-    console.log('hello')
+    transition.style.display = 'flex'
+    transitionTrigger.click()
     sessionStorage.setItem('Loaded', 'True')
   } else {
-    console.log('Goodbye')
   }
 })
