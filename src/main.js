a5288cfa5661506a61f59dd60ad4f6875d16a8d1
tkubata <tkubata9@gmail.com>
2023-04-22 21:40:38 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let transition = document.querySelector('.transition')
 window.addEventListener('load', () => {
   if (sessionStorage.length === 0) {
     console.log('hello')
+    sessionStorage.setItem('Loaded', 'True')
   } else {
     console.log('Goodbye')
   }
