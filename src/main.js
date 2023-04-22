e3a1903fcd2acea934397500004b6f6bb1b02a16
tkubata <tkubata9@gmail.com>
2023-04-22 21:24:39 +0200

    V1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
-
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+sessionStorage.setItem('key', 'value')
